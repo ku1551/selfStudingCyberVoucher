39b7f66ec307a13f89c37642d8fbbffa76f3b9d2
ku1551 <ku1551@hanmail.net>
2021-10-31 14:49:28 +0900

    0-100'

---
+++ src/test1/AlgoRithsm.java
@@ -0,0 +1,28 @@
+package test1;
+
+import java.util.Scanner;
+
+public class AlgoRithsm {
+	public static void main(String[] args) {
+		
+		/*How to add 0 to 100 shortest way */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = Integer.parseInt(sc.next());
+		int result =0;
+		switch(num) {
+			case(0) : result = (100*50)+50;
+					  break;
+			case(1) : for(int i=0; i<=100; i++){
+						result += i;			
+			          };
+					  break;
+		}
+		
+		System.out.println(result);
+		
+		
+	}
+
+}
